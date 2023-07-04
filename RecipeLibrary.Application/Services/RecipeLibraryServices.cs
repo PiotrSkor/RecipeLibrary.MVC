19b5d3d4d7cf822c9d67b67e1dac149ed7d09b5c
Piotr Skorulski <pskorulski@edu.cdv.pl>
2023-07-04 17:09:56 +0200

    Display recipes

--- RecipeLibrary.Application/Services/RecipeLibraryServices.cs
+++ RecipeLibrary.Application/Services/RecipeLibraryServices.cs
@@ -1,20 +1,21 @@
-﻿using FireSharp.Config;
+﻿using FireSharp;
+using FireSharp.Config;
 using FireSharp.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FireSharp.Response;
+using Google.Cloud.Firestore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RecipeLibrary.Domain.Entities;
+
 
 namespace RecipeLibrary.Application.Services
 {
     public class RecipeLibraryServices
     {
+
         public static void Create(RecipeDetails recipeDetails)
         {
-            if (recipeDetails.RecipeName != null)
+            if (recipeDetails.Name != null)
             {
                 string authSecret = "AgmJVhSXQ3hNPwu6Fti6QstWbKZeB4cLLxGIrQqK"; //StringHelper.GetAppSettings(Constants.AppSetingTypes.Firebase, "AuthSecret");
                 string basePath = "https://recipelibrary-d8e55-default-rtdb.europe-west1.firebasedatabase.app/";//StringHelper.GetAppSetings(Constants.AppSetingTypes.Firebase, "BasePath");
@@ -26,13 +27,78 @@ namespace RecipeLibrary.Application.Services
                     BasePath = basePath
                 };
                 IFirebaseClient client = new FireSharp.FirebaseClient(config);
-             
+
                 if (client != null && !string.IsNullOrEmpty(authSecret) && !string.IsNullOrEmpty(basePath))
                 {
                     recipeDetails.EncodingName();
                     client.Push("Recipes/", recipeDetails);
                 }
             }
+        }
+
+
+        public static async Task<List<RecipeDetails>> ReadDataFromFirebase()
+        {
+            string authSecret = "AgmJVhSXQ3hNPwu6Fti6QstWbKZeB4cLLxGIrQqK"; //StringHelper.GetAppSettings(Constants.AppSetingTypes.Firebase, "AuthSecret");
+            string basePath = "https://recipelibrary-d8e55-default-rtdb.europe-west1.firebasedatabase.app/";//StringHelper.GetAppSetings(Constants.AppSetingTypes.Firebase, "BasePath");
+            string senderAppName = "RecipeLibrary";//StringHelper.GetAppSetings(Constants.AppSetingTypes.Firebase, "SenderApplicationName");
+
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = authSecret,
+                BasePath = basePath
+            };
+            IFirebaseClient client = new FireSharp.FirebaseClient(config);
+
+
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Recipes/");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var recipes = new List<RecipeDetails>();
+            if (data != null)
+            {
+                foreach (var item in data)
+                {
+                    recipes.Add(JsonConvert.DeserializeObject<RecipeDetails>(((JProperty)item).Value.ToString()));
+                }
+            }
+
+            return recipes;
+        }
+
+        public static RecipeDetails GetByEncodedName(List<RecipeDetails> encodedName)
+        {
+            string authSecret = "AgmJVhSXQ3hNPwu6Fti6QstWbKZeB4cLLxGIrQqK"; //StringHelper.GetAppSettings(Constants.AppSetingTypes.Firebase, "AuthSecret");
+            string basePath = "https://recipelibrary-d8e55-default-rtdb.europe-west1.firebasedatabase.app/";//StringHelper.GetAppSetings(Constants.AppSetingTypes.Firebase, "BasePath");
+            string senderAppName = "RecipeLibrary";//StringHelper.GetAppSetings(Constants.AppSetingTypes.Firebase, "SenderApplicationName");
+
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = authSecret,
+                BasePath = basePath
+            };
+            IFirebaseClient client = new FirebaseClient(config);
+
+
+            client = new FirebaseClient(config);
+            FirebaseResponse response = client.Get("Recipes/");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var recipes = new RecipeDetails();
+
+            foreach (var item in data)
+            {
+                if (encodedName == item.EncodedName)
+                {
+                    recipes.Name= item.Name;
+                    recipes.DescriptionShort= item.Description;
+                    recipes.DescriptionLong= item.Description;
+                    recipes.IngredientName= item.IngredientName;
+                    recipes.IngredientValue= item.IngredientValue;
+                    recipes.CreatedAt= item.CreatedAt;
+                }
+            }
+
+            return recipes;
         }
     }
 }
--- RecipeLibrary.MVC/Controllers/RecipeController.cs
+++ RecipeLibrary.MVC/Controllers/RecipeController.cs
@@ -1,23 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RecipeLibrary.Application.Services;
+using RecipeLibrary.Domain.Entities;
 
 namespace RecipeLibrary.MVC.Controllers
 {
     public class RecipeController : Controller
     {
         [HttpPost]
-        public IActionResult Create(Domain.Entities.RecipeDetails recipeDetails)
+        public IActionResult Create(RecipeDetails recipeDetails)
         {
             if (ModelState.IsValid)
             {
                 return View(recipeDetails);
             }
-            Application.Services.RecipeLibraryServices.Create(recipeDetails);
+            RecipeLibraryServices.Create(recipeDetails);
             return RedirectToAction(nameof(Create));
         }
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var recipes = await RecipeLibraryServices.ReadDataFromFirebase();
+            return View(recipes);
+        }
+
+        public async Task<IActionResult> Details()
+        {
+            var x = await RecipeLibraryServices.ReadDataFromFirebase();
+
+            var recipe = RecipeLibraryServices.GetByEncodedName(x);
+            
+
+            return View(recipe);
         }
     }
 }
