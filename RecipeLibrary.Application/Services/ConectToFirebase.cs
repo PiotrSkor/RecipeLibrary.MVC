955ebf14417fdbb872871d54d27c9507a3217fff
Piotr Skorulski <pskorulski@edu.cdv.pl>
2023-09-03 01:22:52 +0200

    register added

---
+++ RecipeLibrary.Application/Services/ConectToFirebase.cs
@@ -0,0 +1,26 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp;
+using FirebaseAdmin.Auth;
+
+namespace RecipeLibrary.Application.Services
+{
+    public class ConectToFirebase
+    {
+        public static IFirebaseClient ConectToFirebaseMethod()
+        {
+
+            string authSecret = "AgmJVhSXQ3hNPwu6Fti6QstWbKZeB4cLLxGIrQqK"; //StringHelper.GetAppSettings(Constants.AppSetingTypes.Firebase, "AuthSecret");
+            string basePath = "https://recipelibrary-d8e55-default-rtdb.europe-west1.firebasedatabase.app/";//StringHelper.GetAppSetings(Constants.AppSetingTypes.Firebase, "BasePath");
+
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = authSecret,
+                BasePath = basePath,
+            };
+
+            
+            return new FirebaseClient(config);
+        }
+    }
+}
